a9841e75bb0321de50f51cc555294adb4614a51d
davealone69-gif <davealone69@gmail.com>
2026-07-07 07:15:51 +0800

    Autonomous App Build Factory v3.0 - Cycle 3 Attempt 10

--- app/src/main/java/com/example/droidcraft/MainActivity.kt
+++ app/src/main/java/com/example/droidcraft/MainActivity.kt
@@ -51,7 +51,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Using default MaterialTheme which picks up the theme defined in Manifest
             MaterialTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     HabitTrackerScreen()
