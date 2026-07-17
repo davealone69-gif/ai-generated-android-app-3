18c6e6f93ffdc3416f3598e92132aff7df39ab72
davealone69-gif <davealone69@gmail.com>
2026-07-17 19:59:07 +0800

    Autonomous App Build Factory v3.0 - Cycle 1 Attempt 3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,5 +13,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Mandela vs Matrix Re-Imaginator A to APK"
-include(":app")
-project(":app").projectDir = file("App")
+include(":App")
